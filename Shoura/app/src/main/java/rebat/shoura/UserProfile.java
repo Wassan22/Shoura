77bc128f7d2b1e80d391c27628bd0fb3bb9c505d
wassanalluhaidan <Wassan-Alluhaidan@live.com>
2017-02-15 18:05:20 +0300

    Navigation and Project Structure Base

---
+++ Shoura/app/src/main/java/rebat/shoura/UserProfile.java
@@ -0,0 +1,16 @@
+package rebat.shoura;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Maram on 2/15/17.
+ */
+
+public class UserProfile extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+    }
+}
